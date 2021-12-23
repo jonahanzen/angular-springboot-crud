633b447c622f7590d83f4ce5481ae85e934fb355
Jonny <jona_hanzen@hotmail.com>
2021-12-23 16:42:36 -0300

    put method implemented

--- src/main/java/br/com/controller/EmployeeController.java
+++ src/main/java/br/com/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +38,12 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee) {
         return repository.save(employee);
     }
+    
+    @PutMapping("/employees/{id}")
+    public Employee updateEmployeeById(@PathVariable Long id,@RequestBody Employee employeeDetails) {
+    	Employee employee = repository.findById(id).get();
+    	employeeDetails.setId(employee.getId());
+    	return repository.save(employeeDetails);
+    	}
 
 }
