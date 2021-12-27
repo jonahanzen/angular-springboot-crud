6fc4606552c819c4e611e43099daacbc46de06c1
Jonny <jona_hanzen@hotmail.com>
2021-12-27 08:44:17 -0300

    delete employee implemented

--- src/main/java/br/com/controller/EmployeeController.java
+++ src/main/java/br/com/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,5 +46,11 @@ public class EmployeeController {
     	employeeDetails.setId(employee.getId());
     	return repository.save(employeeDetails);
     	}
+    
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployeeById(@PathVariable Long id) {
+    	Employee employee = repository.findById(id).get();
+    	repository.delete(employee);
+    }
 
 }
